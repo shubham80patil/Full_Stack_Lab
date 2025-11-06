be785216e23881a920d11d550d25d4d69c2141a2
shubham80patil <166232026+shubham80patil@users.noreply.github.com>
2025-11-06 23:33:10 +0530

    Add files via upload

---
+++ doctors.js
@@ -0,0 +1,66 @@
+// Mock Doctors API Data
+export const doctorsData = [
+  {
+    id: 1,
+    name: "Dr. Aditi Sharma",
+    department: "Cardiology",
+    qualification: "MD, DM (Cardiology)",
+    image: "https://randomuser.me/api/portraits/women/44.jpg"
+  },
+  {
+    id: 2,
+    name: "Dr. Rajesh Patel",
+    department: "Cardiology",
+    qualification: "MBBS, MD (Cardiology)",
+    image: "https://randomuser.me/api/portraits/men/46.jpg"
+  },
+  {
+    id: 3,
+    name: "Dr. Meena Nair",
+    department: "Cardiology",
+    qualification: "MBBS, DNB (Cardiology)",
+    image: "https://randomuser.me/api/portraits/women/65.jpg"
+  },
+  {
+    id: 4,
+    name: "Dr. Sameer Khan",
+    department: "Neurology",
+    qualification: "MBBS, DM (Neurology)",
+    image: "https://randomuser.me/api/portraits/men/32.jpg"
+  },
+  {
+    id: 5,
+    name: "Dr. Priya Verma",
+    department: "Neurology",
+    qualification: "MBBS, MD (Neurology)",
+    image: "https://randomuser.me/api/portraits/women/31.jpg"
+  },
+  {
+    id: 6,
+    name: "Dr. Abhinav Joshi",
+    department: "Neurology",
+    qualification: "MBBS, DNB (Neurology)",
+    image: "https://randomuser.me/api/portraits/men/51.jpg"
+  },
+  {
+    id: 7,
+    name: "Dr. Ritu Singh",
+    department: "Orthopedics",
+    qualification: "MBBS, MS (Ortho)",
+    image: "https://randomuser.me/api/portraits/women/71.jpg"
+  },
+  {
+    id: 8,
+    name: "Dr. Arjun Deshmukh",
+    department: "Orthopedics",
+    qualification: "MBBS, DNB (Ortho)",
+    image: "https://randomuser.me/api/portraits/men/27.jpg"
+  },
+  {
+    id: 9,
+    name: "Dr. Kiran Rao",
+    department: "Orthopedics",
+    qualification: "MBBS, MS (Ortho)",
+    image: "https://randomuser.me/api/portraits/women/23.jpg"
+  },
+];
